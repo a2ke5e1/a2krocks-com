6c8c89006a454b160df1cf299e158189c773d33c
a2krocks <aakapurv@gmail.com>
2022-02-14 13:10:38 +0530

    Added Layout to keep appbar and footer

---
+++ components/layout.tsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Head from 'next/head'
+import Footer from '../components/footer'
+import ResponsiveAppBar from '../components/ResponsiveAppBar'
+
+// Theme stuff
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+export default function Layout({ children }) {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div>
+                <Head>
+                    <title>Apurv Ajay Kumar</title>
+                    <meta name="description" content="a2krocks' portfolio made using next.js" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <main>
+                    <ResponsiveAppBar />
+                    {children}
+                </main>
+                <footer>
+                    <Footer />
+                </footer>
+            </div>
+        </ThemeProvider>
+    );
+}
+
---
+++ pages/about.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Footer from '../components/footer'
+import ResponsiveAppBar from '../components/ResponsiveAppBar'
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>About - Apurv Ajay Kumar</title>
+            </Head>
+            <main>
+                
+            </main>
+        </div>
+    )
+}
+
+export default About
