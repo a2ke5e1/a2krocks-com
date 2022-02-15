fd83fbdac4f78ac2f16d8c8bcc857b5c618c0f0e
a2krocks <aakapurv@gmail.com>
2022-02-15 21:57:46 +0530

    Fixed Escape Characters

---
+++ pages/projects/3.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import { Container} from "@mui/material"
+
+const ProjectYearlyProgress : NextPage = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default ProjectYearlyProgress
