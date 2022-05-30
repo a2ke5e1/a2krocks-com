055c23673dc7a058f906a9fcd450161daccd6596
a2krocks <aakapurv@gmail.com>
2022-05-30 16:07:28 +0530

    Added Build and Hosted On

--- components/v2/FooterV2.tsx
+++ components/v2/FooterV2.tsx
@@ -1,11 +1,12 @@
 import type {NextPage} from 'next'
 
-import {Box, Typography} from '@mui/material'
+import {Box, Grid, Typography} from '@mui/material'
 import SocialMedia from "../v2/SocialMedia";
 import SocialMediaUrls from "../../Info/SocialMediaUrls";
 import AboutMe from "../../Info/AboutMe";
 import Link from "next/link";
 import styles from "../../styles/components/v2/footer.module.css"
+import Image from "next/image";
 
 const smUrl = new SocialMediaUrls();
 const abtMe = new AboutMe();
@@ -14,7 +15,7 @@ const abtMe = new AboutMe();
 // Left Side of the footer where all my social media info belongs
 const SocialMediaSide = (
     <Box>
-        <Typography textAlign="left" fontWeight="bold" sx={{ml: 1.5, pt: 2}}>
+        <Typography textAlign="left" fontWeight="bold" sx={{ml: 1.5}}>
             {abtMe.mName}
         </Typography>
         <Link href={smUrl.mail}>
@@ -26,12 +27,42 @@ const SocialMediaSide = (
     </Box>
 )
 
+const HostedOn = (
+    <Box className={styles.hosted_on} sx={{ml: 1.5}}>
+        <Typography textAlign="left" fontWeight="bold">
+            Hosted On
+        </Typography>
+        <Image className={styles.vercel_logo} src="/vercel.svg" width={150} height={50}/>
+    </Box>
+)
+
+
+const BuildWith = (
+    <Box className={styles.hosted_on} sx={{ml: 1.5}}>
+        <Typography textAlign="left" fontWeight="bold">
+            Build With
+        </Typography>
+        <Image className={styles.vercel_logo} src="/nextjs.png" width={80} height={80}/>
+    </Box>
+)
 
 const Footer: NextPage = () => {
 
     return (
         <Box className={styles.background}>
-            {SocialMediaSide}
+            <Grid container spacing={4}>
+                <Grid item xs={9.8}>
+                    {SocialMediaSide}
+                </Grid>
+
+                <Grid item >
+                    {HostedOn}
+                </Grid>
+                <Grid item >
+                    {BuildWith}
+                </Grid>
+
+            </Grid>
         </Box>
     )
 }
