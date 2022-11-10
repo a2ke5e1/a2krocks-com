02b14ca506c5a509b3b8c4806107413a22b8e330
Apurv Ajay Kumar <aakapurv@gmail.com>
2022-11-10 23:46:23 +0530

    Updated to Nextjs 13

--- components/v2/FooterV2.tsx
+++ components/v2/FooterV2.tsx
@@ -14,12 +14,12 @@ const abtMe = new AboutMe();
 
 // Left Side of the footer where all my social media info belongs
 const SocialMediaSide = (
-    <Box sx={{ pt: 5, pb : 5 }} >
-        <Typography textAlign="left" fontWeight="bold" sx={{ }}>
+    <Box sx={{ pt: 5, pb: 5 }} >
+        <Typography textAlign="left" fontWeight="bold" sx={{}}>
             {abtMe.mName}
         </Typography>
         <Link href={smUrl.mail}>
-            <Typography textAlign="left" fontWeight="lighter" sx={{  mb: 2 }}>
+            <Typography textAlign="left" fontWeight="lighter" sx={{ mb: 2 }}>
                 {abtMe.mail}
             </Typography>
         </Link>
@@ -27,24 +27,24 @@ const SocialMediaSide = (
     </Box>
 )
 
-const HostedOn = (
-    <Box className={styles.hosted_on} sx={{ ml: 1.5 }}>
-        <Typography textAlign="left" fontWeight="lighter">
-            Hosted On
-        </Typography>
-        <Image className={styles.vercel_logo} src="/vercel.svg" width={150} height={50} />
-    </Box>
-)
+// const HostedOn = (
+//     <Box className={styles.hosted_on} sx={{ ml: 1.5 }}>
+//         <Typography textAlign="left" fontWeight="lighter">
+//             Hosted On
+//         </Typography>
+//         <Image className={styles.vercel_logo} src="/vercel.svg" width={150} height={50} />
+//     </Box>
+// )
 
 
-const BuildWith = (
-    <Box className={styles.hosted_on} sx={{ ml: 1.5 }}>
-        <Typography textAlign="left" fontWeight="lighter">
-            Build With
-        </Typography>
-        <Image className={styles.vercel_logo} src="/nextjs.png" width={80} height={80} />
-    </Box>
-)
+// const BuildWith = (
+//     <Box className={styles.hosted_on} sx={{ ml: 1.5 }}>
+//         <Typography textAlign="left" fontWeight="lighter">
+//             Build With
+//         </Typography>
+//         <Image className={styles.vercel_logo} src="/nextjs.png" width={80} height={80} />
+//     </Box>
+// )
 
 const Footer: NextPage = () => {
 
